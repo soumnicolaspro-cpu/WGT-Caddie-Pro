64da0717062fb94f4a0a9dfb294d55f5c6481a0e
soumnicolaspro-cpu <soum.nicolas.pro@gmail.com>
2026-07-14 02:22:13 +0200

    Correction puissance

--- app.js
+++ app.js
@@ -12,18 +12,49 @@ document.getElementById("calculer").addEventListener("click", function () {
     distanceCorrigee -= (temperature - 20) * 0.5;
 
     let club = "";
-    let puissance = 100;
+let puissance = 100;
 
-    if (distanceCorrigee >= 180) club = "Driver";
-    else if (distanceCorrigee >= 170) club = "Bois 3";
-    else if (distanceCorrigee >= 160) club = "Fer 4";
-    else if (distanceCorrigee >= 150) club = "Fer 5";
-    else if (distanceCorrigee >= 140) club = "Fer 6";
-    else if (distanceCorrigee >= 130) club = "Fer 7";
-    else if (distanceCorrigee >= 120) club = "Fer 8";
-    else if (distanceCorrigee >= 110) club = "Fer 9";
-    else if (distanceCorrigee >= 90) club = "Pitch";
-    else club = "Sand Wedge";
+if (distanceCorrigee >= 180) {
+    club = "Driver";
+    puissance = Math.round((distanceCorrigee / 180) * 100);
+}
+else if (distanceCorrigee >= 170) {
+    club = "Bois 3";
+    puissance = Math.round((distanceCorrigee / 170) * 100);
+}
+else if (distanceCorrigee >= 160) {
+    club = "Fer 4";
+    puissance = Math.round((distanceCorrigee / 160) * 100);
+}
+else if (distanceCorrigee >= 150) {
+    club = "Fer 5";
+    puissance = Math.round((distanceCorrigee / 150) * 100);
+}
+else if (distanceCorrigee >= 140) {
+    club = "Fer 6";
+    puissance = Math.round((distanceCorrigee / 140) * 100);
+}
+else if (distanceCorrigee >= 130) {
+    club = "Fer 7";
+    puissance = Math.round((distanceCorrigee / 130) * 100);
+}
+else if (distanceCorrigee >= 120) {
+    club = "Fer 8";
+    puissance = Math.round((distanceCorrigee / 120) * 100);
+}
+else if (distanceCorrigee >= 110) {
+    club = "Fer 9";
+    puissance = Math.round((distanceCorrigee / 110) * 100);
+}
+else if (distanceCorrigee >= 90) {
+    club = "Pitch";
+    puissance = Math.round((distanceCorrigee / 90) * 100);
+}
+else {
+    club = "Sand Wedge";
+    puissance = Math.round((distanceCorrigee / 70) * 100);
+}
+
 
     document.getElementById("club").textContent = club;
     document.getElementById("puissance").textContent = puissance + " %";
